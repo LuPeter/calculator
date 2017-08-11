eaa4f9c9acca5b9c4523aac7a8b052aaf49f60b8
LuPeter <p23609857@hotmail.com>
2017-08-12 05:57:31 +0800

    basic caculator

---
+++ docs/javascripts/scripts.js
@@ -0,0 +1,113 @@
+var str=""; //暫存輸入的值(string)
+var op="+";  //暫存oprator
+var number=0;
+//存取數值
+function stor(i){
+    str+=i;
+    document.getElementById("output").innerHTML=str;
+}
+/*
+預設 number為0，op為+
+輸入數值在str，按下任何運算符號時，先將str值用op運算到number，再將新的運算傳給op，最後把str清空
+如果連續按不同運算符號，此時str為空，將新的運算傳給op
+*/
+function method(operator){
+    if(str==""){
+        op=operator;
+    }
+    else{
+        switch(op){
+            case '+':
+                number+=Number(str);
+                console.log(number);
+                break;
+            case '-':
+                number-=Number(str);
+                console.log(number);
+                break;
+            case 'x':
+                (number==0)?number=Number(str):number*=Number(str);
+                console.log(number);
+                break;
+            case '/':
+                (number==0)?number=Number(str):number/=Number(str);
+                console.log(number);
+                break;
+            default:
+                break;    
+        }     
+        op=operator;
+        console.log(op);
+        str=""; 
+        document.getElementById("output").innerHTML=number;
+    }       
+}
+//將number用目前的op計算出來，並將op設回預設的'+'
+function equal(){
+    method(op);
+    str=number.toString();
+    number=0;
+    console.log(op);
+}
+//僅將str清空
+function clearError(){
+    str="";
+    document.getElementById("output").innerHTML= "0";
+}
+//將所有數值初始化
+function clearAll(){    
+    number=0;
+    str="";
+    op="+";
+    document.getElementById("output").innerHTML= number;
+}
+//將字串往右移一格
+function shiftstr(){        
+    str=str.slice(0,str.length-1);
+    if(str==''){
+        document.getElementById("output").innerHTML= '0';
+    }
+    else{
+        document.getElementById("output").innerHTML= str;
+    }
+}
+//改變str正負號
+function sign(){
+    if(str!=''){
+        if(str[0]=='-'){
+            str=str.slice(1);
+        }
+        else{            
+            str='-'+str;
+        }
+        document.getElementById("output").innerHTML= str;
+    }
+}
+//開根號
+function tosqrt(){
+    str=Math.sqrt(Number(str));    
+    str=str.toString();
+    console.log('sqrt= '+str);
+    document.getElementById("output").innerHTML = str;
+}
+//平方
+function square(){
+    str=Math.pow(Number(str),2);
+    str=str.toString();
+    console.log('square= '+str);
+    document.getElementById("output").innerHTML = str;
+}
+//倒數
+function recip(){
+    str=1/Number(str);
+    str=str.toString();
+    console.log('recip= '+str);
+    document.getElementById("output").innerHTML = str;
+}
+//百分比%
+function percent(){
+    if(number!=0 && str!=''){
+        str=Number(str)*number/100;
+        equal();
+    }
+}
